c454595524fa54ba60e63786a53e34d9db2c2771
arnavch10 <arnavchoudhary53@gmail.com>
2026-07-08 09:49:42 -0400

    feat: call createFreePort() -> no more hardcoded port values

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,20 +1,27 @@
-import { promises as fs } from "node:fs";
 import path from "path";
 import { mkdir, writeFile } from "node:fs/promises";
+import { createFreePort } from "../lib/ports";
 export async function initCommand(options: boolean) {
 
     console.log("init ran", options)
     try {
+        const proxyPort = await createFreePort(); // ts proxy port
+        let memoryPort = await createFreePort(); // python memory engine port
+
+        while (memoryPort === proxyPort) {
+            memoryPort = await createFreePort();
+        }
+
+        
         // creating file
-        const data = JSON.stringify({version: "0.1.0"})
+        const data = JSON.stringify({version: "0.1.0", proxyPort, memoryPort})
         const projectFolder = ".memory";
         const createDir = await mkdir(projectFolder, { recursive: true }); // using true to not throw error if directory already exists
         
         // config path
         const configPath = path.join(projectFolder, "config.json");
 
-        const configFile = await writeFile(configPath, data, "utf8");
-        await configFile
+        await writeFile(configPath, data, "utf8");
         // checking if the memory folder already exists
         if (createDir !== undefined) {
             console.log(`${projectFolder} created`)
