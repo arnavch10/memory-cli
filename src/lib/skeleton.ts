c43b42a5586c8722d362d066f54d0e29185f1d69
arnavch10 <arnavchoudhary53@gmail.com>
2026-07-28 19:13:12 -0400

    fix: changed param types from "action" to "string"

--- src/lib/skeleton.ts
+++ src/lib/skeleton.ts
@@ -4,22 +4,27 @@ import { which } from "bun";
 import * as fs from "fs";
 import { supportedTools } from "../lib/anthropic/format"
 
-async function checkFileExists(filepath: Action): Promise<boolean> {
+async function checkFileExists(filepath: string): Promise<boolean> {
     try {
-        await fs.promises.access(filepath.input.file_path, fs.constants.F_OK);
+        await fs.promises.access(filepath, fs.constants.F_OK);
         return true;
     } catch (e) {
         return false;
     }
 }
 
-async function getActionFilePath(action: Action) {
-    return await checkFileExists(action) ? action.input.file_path : undefined;
+async function getActionFilePath(action: Action): Promise<string | undefined> {
+    const filepath = action.input?.file_path;
+
+    if (!filepath) {
+        return undefined;
+    }
+    return await checkFileExists(filepath) ? filepath : undefined;
 }
 
-async function getCtagSymbols(filepath: Action) {
+async function getCtagSymbols(filepath: string) {
 
-    if (!checkFileExists(filepath)) {
+    if (!(await checkFileExists(filepath))) {
         return [];
     }
 
@@ -28,7 +33,7 @@ async function getCtagSymbols(filepath: Action) {
 
     if (isCtagsInstalled) {
         // starting ctags process
-        const process = Bun.spawn(["ctags", "--output-format=json", "-f", "-", filepath.input.file_path]);
+        const process = Bun.spawn(["ctags", "--output-format=json", "-f", "-", filepath]);
 
         const output = await new Response(process.stdout).text();
         const lines = output.split(/\r?\n/).filter(line => line.trim() !== ""); // split into new lines and filter out any blank space
@@ -43,13 +48,14 @@ async function getCtagSymbols(filepath: Action) {
         await process.exited;
         console.log(output)
         return tags;
+
     } else {
         return [];
     }
 
 }
 
-async function createActionsSkeleton(actions: Action[]) {
+export async function createActionsSkeleton(actions: Action[]) {
     if (actions.length) {
         for (const a of actions) {
            if (!supportedTools.includes(a.tool)) {
@@ -66,7 +72,8 @@ async function createActionsSkeleton(actions: Action[]) {
             continue;
            } else {
             try {
-                const symbols = getCtagSymbols(path)
+                const symbols = await getCtagSymbols(path)
+                a.symbols = symbols;
                 console.log(symbols)
             } catch (e) {
             console.log(e);
@@ -76,4 +83,6 @@ async function createActionsSkeleton(actions: Action[]) {
 
         }
     }
+
+    return actions;
 }
