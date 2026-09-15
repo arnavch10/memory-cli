6bb1c8c29b48e4c633401fa83fc5a44a59b9785e
arnavch10 <arnavchoudhary53@gmail.com>
2026-09-15 16:54:56 -0400

    refactor: removed unnecessary paths

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -23,6 +23,10 @@ export async function initCommand(options?: boolean): Promise<void>{
         
         const proxyPort = await createFreePort(); // ts proxy port
         let memoryPort = await createFreePort(); // python memory engine port
+        // if memory port is the same as proxyport generate a new port for memory
+        while (memoryPort === proxyPort) {
+            memoryPort = await createFreePort();
+        }
         const config = {
             version: 1,
             createdAt: new Date().toString(),
@@ -34,23 +38,22 @@ export async function initCommand(options?: boolean): Promise<void>{
         }
 
         const ledger = { version: 1, entries: [] }
-        while (memoryPort === proxyPort) {
-            memoryPort = await createFreePort();
-        }
-
+        const codexConfig = "";
         // check whether config and ledger already exist
         if (await fileExists(configPath) && await fileExists(ledgerPath)) {
             console.log("Config and ledger already exist!");
+            return;
         }
         
-        // creating file
-        const data = JSON.stringify(config, null, 2);
+        // ledger and config data
+        const configData = JSON.stringify(config, null, 2);
         const ledgeData = JSON.stringify(ledger, null, 2);
 
         const createDir = await mkdir(projectFolder, { recursive: true }); // using true to not throw error if directory already exists
         
-        await writeFile(configPath, data, "utf8");
+        await writeFile(configPath, configData, "utf8");
         await writeFile(ledgerPath, ledgeData, "utf8");
+
         // checking if the memory folder already exists
         if (createDir !== undefined) {
             console.log(`${projectFolder} created`)
